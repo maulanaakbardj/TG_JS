f3abe77a45d0b2da5bc4d026690fe5a8bdc36126
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2022-03-09 00:45:39 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const cred = {
+    host: "mytg.i.tgcloud.io",
+    graph: 'Entity_Resolution',
+    username: 'tigergraph',
+    password: 'pass123',
+    secret: '0pm76dngq2729t9e5appa7i3m3us5rps',
+    token: 'p1i4dqgcvmvvonpfrt2470vn3lkurs4p'
+}
+
+module.exports = cred;
